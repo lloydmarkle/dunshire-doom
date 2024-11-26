411821e4a47ddd06ff8c2cacbcb87dac4815a1de
Lloyd Markle <lloyd.markle@gmail.com>
2024-11-25 23:43:18 -0800

    add bundle visualizer to see the impact of dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    visualizer({ filename: 'bundle-stats.html' }),
+  ],
   server: {
     host: '0.0.0.0',
   },
